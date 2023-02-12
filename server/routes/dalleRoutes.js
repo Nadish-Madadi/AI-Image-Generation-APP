4d4dd1ff2c7830fd68fb23fd7942ac2200eaa6a4
Nadish-Madadi <75193860+Nadish-Madadi@users.noreply.github.com>
2023-02-12 12:13:43 -0500

    updated server side code - mongodb and routes

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+router.route('/').get((req,res)=> {
+  res.send('Hello from AI!');
+})
+
+router.route('/').post(async(req,res)=> {
+  try{
+    const {prompt} = req.body;
+    const aiResponse = await openai.createImage({
+      prompt,
+      n:1,
+      size:'1024x1024',
+      response_format:'b64_json',
+    });
+    const image = aiResponse.data.data[0].b_64_json;
+
+    res.status(200).json({photo:image});
+  }catch(error){
+    console.log(error);
+    res.status(500).send(error?.response.data.error.message);
+  }
+})
+
+export default router;
